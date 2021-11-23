ade61672cbfbf91dc9f7a825a08b0c126ec97728
Salvador <salvadororozcogon511@gmail.com>
2021-11-23 10:28:44 -0600

    I added while.py

---
+++ while.py
@@ -0,0 +1,12 @@
+def run():
+    a = int(input('Until what number should I count? '))
+    b = 0
+
+    while(b != a):
+        b =  b+1
+        print(int(b))
+
+
+
+if __name__ == '__main__':
+    run()
